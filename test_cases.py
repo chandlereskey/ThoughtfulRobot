9256c3fc1aad00cd26ef3b764608d9b15c62c9d7
chandlereskey <99619563+chandlereskey@users.noreply.github.com>
2024-12-15 22:25:16 -0500

    Create test_cases.py

---
+++ test_cases.py
@@ -0,0 +1,23 @@
+import unittest
+
+class TestPackageSorter(unittest.TestCase):
+  def test_sort_standard(self):
+    self.assertEqual(sort(10, 10, 10, 10), "STANDARD")
+
+  def test_sort_special(self):
+    # bulky because of width
+    self.assertEqual(sort(150, 10, 10, 10), "SPECIAL")
+    # bulky because of height
+    self.assertEqual(sort(10, 150, 10, 10), "SPECIAL")
+    # bulky because of length
+    self.assertEqual(sort(10, 10, 150, 10), "SPECIAL")
+    # bulky because of volume
+    self.assertEqual(sort(100, 100, 100, 10), "SPECIAL")
+
+    # heavy
+    self.assertEqual(sort(10, 10, 10, 20), "SPECIAL")
+
+  def test_sort_rejected(self):
+    self.assertEqual(sort(150, 10, 10, 20), "REJECTED")
+
+unittest.main()
